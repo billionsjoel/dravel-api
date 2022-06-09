72df2c6655677c9f61ff091cb23f7196920f7b25
uae <aniekaudo@yahoo.com>
2022-06-09 14:02:23 +0100

    :sparkles: Seed
    Create trip in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Trip.create(name: "Vacation", destination: "Abuja", vip: true, price: 4.7, image: "https://images.unsplash.com/photo-1520046045453-547e1175391e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", start_date: "2017-01-01", end_date: "2017-01-02", description: "This is a trip to Abuja")
+
+Trip.create(name: "Vacation", destination: "Abuja", vip: true, price: 4.7, image: "https://images.unsplash.com/photo-1520046045453-547e1175391e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", start_date: "2017-01-01", end_date: "2017-01-02", description: "This is a trip to Abuja")
+
+Trip.create(name: "Vacation", destination: "Abuja", vip: true, price: 4.7, image: "https://images.unsplash.com/photo-1520046045453-547e1175391e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", start_date: "2017-01-01", end_date: "2017-01-02", description: "This is a trip to Abuja")
+
+Trip.create(name: "Vacation", destination: "Abuja", vip: true, price: 4.7, image: "https://images.unsplash.com/photo-1520046045453-547e1175391e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", start_date: "2017-01-01", end_date: "2017-01-02", description: "This is a trip to Abuja")
+
+Trip.create(name: "Vacation", destination: "Abuja", vip: true, price: 4.7, image: "https://images.unsplash.com/photo-1520046045453-547e1175391e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", start_date: "2017-01-01", end_date: "2017-01-02", description: "This is a trip to Abuja")
+
+Trip.create(name: "Vacation", destination: "Abuja", vip: true, price: 4.7, image: "https://images.unsplash.com/photo-1520046045453-547e1175391e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", start_date: "2017-01-01", end_date: "2017-01-02", description: "This is a trip to Abuja")
+
+Trip.create(name: "Vacation", destination: "Abuja", vip: true, price: 4.7, image: "https://images.unsplash.com/photo-1520046045453-547e1175391e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", start_date: "2017-01-01", end_date: "2017-01-02", description: "This is a trip to Abuja")
+
+Trip.create(name: "Vacation", destination: "Abuja", vip: true, price: 4.7, image: "https://images.unsplash.com/photo-1520046045453-547e1175391e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", start_date: "2017-01-01", end_date: "2017-01-02", description: "This is a trip to Abuja")
