ff927a6fa3c9ecd5dfe9337300b59aa5cf93e929
uae <aniekaudo@yahoo.com>
2022-06-09 14:11:56 +0100

    :bug: Seed
    Created trip in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,9 +9,9 @@
 
 Trip.create(name: "Vacation", destination: "Abuja", vip: true, price: 4.7, image: "https://images.unsplash.com/photo-1520046045453-547e1175391e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", start_date: "2017-01-01", end_date: "2017-01-02", description: "This is a trip to Abuja")
 
-Trip.create(name: "Vacation", destination: "Abuja", vip: true, price: 4.7, image: "https://images.unsplash.com/photo-1520046045453-547e1175391e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", start_date: "2017-01-01", end_date: "2017-01-02", description: "This is a trip to Abuja")
+Trip.create(name: "Business", destination: "Los Angeles", vip: true, price: 50.7, image: "https://images.unsplash.com/photo-1618694149123-4c95a5cd0657?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fGJ1c3luZXNzJTIwc3RyaXAlMjBwbGFjZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60", start_date: "2018-04-01", end_date: "2018-05-02", description: "This is a business trip to Los Angeles")
 
-Trip.create(name: "Vacation", destination: "Abuja", vip: true, price: 4.7, image: "https://images.unsplash.com/photo-1520046045453-547e1175391e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", start_date: "2017-01-01", end_date: "2017-01-02", description: "This is a trip to Abuja")
+Trip.create(name: "Pilgrimage", destination: "Vatican", vip: true, price: 100.7, image: "https://images.unsplash.com/photo-1616369415526-ce055dfa3a87?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MXw0Njk2OTYxfHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500&q=60", start_date: "2019-02-01", end_date: "2019-09-02", description: "This is a trip to Vatican")
 
 Trip.create(name: "Vacation", destination: "Abuja", vip: true, price: 4.7, image: "https://images.unsplash.com/photo-1520046045453-547e1175391e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80", start_date: "2017-01-01", end_date: "2017-01-02", description: "This is a trip to Abuja")
 
