08159ad1dc0de4cf731580479aea8b0f8a1f4a26
uae <aniekaudo@yahoo.com>
2022-06-11 12:37:39 +0100

    :test_tube: Unit test
    Added unit tests to user routes

---
+++ spec/routing/user_routing_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe UsersController, type: :routing do
+  describe 'routing' do
+    it 'routes to #index' do
+      expect(get: '/users').to route_to('users#index')
+    end
+
+    it 'routes to #new' do
+      expect(get: '/users/sign_in').to route_to('users/sessions#new')
+    end
+
+    it 'routes to #show' do
+      expect(get: '/users/1').to route_to('users#show', id: '1')
+    end
+
+    # it 'routes to #edit' do
+    #   expect(get: '/transactions/1/edit').to route_to('transactions#edit', id: '1')
+    # end
+
+    it 'routes to #create' do
+      expect(post: '/users/sign_in').to route_to('users/sessions#create')
+    end
+
+    # it 'routes to #update via PUT' do
+    #   expect(put: '/transactions/1').to route_to('transactions#update', id: '1')
+    # end
+
+    # it 'routes to #update via PATCH' do
+    #   expect(patch: '/transactions/1').to route_to('transactions#update', id: '1')
+    # end
+
+    it 'routes to #destroy' do
+      expect(delete: '/users/1').to route_to('users#destroy', id: '1')
+    end
+  end
+end
