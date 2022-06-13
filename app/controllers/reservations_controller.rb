0c2fad91a97d2285f80c6a2c6ba41f4e3f6f7f69
joskalenda <joskalenda3@gmail.com>
2022-06-13 10:52:38 +0200

    Modify config.jwt in the  devise.rb file

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -15,8 +15,7 @@ class ReservationsController < ApplicationController
   def show
     # @user = User.find(params[:user_id])
     # @reservation = @user.reservations.find(params[:id])
-    # @reservation = current_user.reservations.find(params[:id])
-    # @reservation = current_user.Trip.find(params[:trip_id]).reservations.find(params[:id])
+    @reservation = current_user.reservations.find(params[:id])
 
     render json: @reservation
   end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -310,7 +310,10 @@ Devise.setup do |config|
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
 
+  # config.jwt do |jwt|
+  #   jwt.secret = Rails.application.credentials.devise[:jwt_secret_key]
+  # end
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.devise[:jwt_secret_key]
+    jwt.secret = 'jwt_secret_key'
   end
 end
