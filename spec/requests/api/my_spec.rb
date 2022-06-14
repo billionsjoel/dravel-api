fccd9eaac8d4088e389c0490086be75de473e6f6
uae <aniekaudo@yahoo.com>
2022-06-14 19:12:48 +0100

    :memo: Swagger
    Created user documentmentation for api

--- spec/requests/api/my_spec.rb
+++ spec/requests/api/my_spec.rb
@@ -2,6 +2,68 @@ require 'swagger_helper'
 
 RSpec.describe 'Dravel API' do
 
+
+  path '/users' do
+
+    post 'Creates a user' do
+      tags 'Users'
+      consumes 'application/json'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          email: { type: :string },
+          user_name: { type: :string },
+          password: { type: :number }
+        },
+        required: [ 'email', 'user_name', 'password' ]
+      }
+
+      response '201', 'user created' do
+        let(:user) { { email: 'arnie@gmail.com', user_name: 'Udo', password: 'udo12345' } }
+        run_test!
+      end
+
+      response '422', 'invalid request' do
+        let(:user) { { name: 'Udo' } }
+        run_test!
+      end
+    end
+  end
+
+  path '/users/{id}' do
+
+    get 'Retrieves a user' do
+      tags 'Users', 'Another Tag'
+      produces 'application/json', 'application/xml'
+      parameter user_name: :id, in: :path, type: :string
+
+      response '200', 'user found' do
+        schema type: :object,
+          properties: {
+            id: { type: :integer },
+            email: { type: :string },
+            user_name: { type: :string },
+            password: { type: :integer },
+          },
+          required: [ 'id', 'email', 'user_name', 'password' ]
+
+        let(:id) { User.create(email: 'arnie@gmail.com', user_name: 'Udo', password: 'udo12345').id }
+        run_test!
+      end
+
+      response '404', 'user not found' do
+        let(:id) { 'invalid' }
+        run_test!
+      end
+
+      response '406', 'unsupported accept header' do
+        let(:'Accept') { 'application/trip' }
+        run_test!
+      end
+    end
+  end
+
+
   path '/trips' do
 
     post 'Creates a trip' do
@@ -37,7 +99,7 @@ RSpec.describe 'Dravel API' do
     get 'Retrieves a trip' do
       tags 'Trips', 'Another Tag'
       produces 'application/json', 'application/xml'
-      parameter name: :id, in: :path, type: :string
+      parameter name: :id, in: :path, type: :integer
 
       response '200', 'trip found' do
         schema type: :object,
