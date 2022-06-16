0a4e231fc92830cc9b614b6ec30abb0e6f19eed0
Aniekan udo <aniekaudo@yahoo.com>
2022-06-16 07:46:56 +0100

    Update registrations_controller.rb

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,13 +2,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
   private
-
+  # rubocop:disable
   def respond_with(resource, _opts = {})
     register_success && return if resource.persisted?
 
     register_failed
   end
-
+  # rubocop:disable
   def register_success
     render json: { message: "#{current_user.user_name} has signed up sucessfully." }, status: :ok
   end
