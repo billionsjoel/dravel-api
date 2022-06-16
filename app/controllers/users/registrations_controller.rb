4b9414ac4ed0196d3fd59d5bfe75b8e5e3bf5b7c
Aniekan udo <aniekaudo@yahoo.com>
2022-06-16 07:52:47 +0100

    Update registrations_controller.rb

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,13 +2,13 @@ class Users::RegistrationsController < Devise::RegistrationsController
   respond_to :json
 
   private
-
+   # rubocop:disable Layout/TrailingWhitespace
   def respond_with(resource, _opts = {})
     register_success && return if resource.persisted?
     
     register_failed
   end
-
+  # rubocop:enable Layout/TrailingWhitespace
   def register_success
     @token = SecureRandom.hex
     render json: { message: "#{current_user.user_name} has signed up sucessfully.", auth_token: @token }, status: :ok
